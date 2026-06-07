2ccd8ae6a972873be3ba5ddc9608f021e190ba9f
ThiMung <dtmung0712@gmail.com>
2026-06-08 00:26:23 +0700

    EMS-94_Complete google authentication feature

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,16 +4,18 @@ export const authService = {
   login: async (credentials) => {
     const response = await api.post('/attendee/login', {
       ...credentials,
-      // Backend dùng role này để chặn đăng nhập nhầm cổng
       required_role: 'attendee',
     });
+    return response.data;
+  },
 
+  googleLogin: async (accessToken) => {
+    const response = await api.post('/attendee/auth/google', { credential: accessToken });
     return response.data;
   },
 
   register: async (data) => {
     const response = await api.post('/attendee/register', data);
-
     return response.data;
   },
 
